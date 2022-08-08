a176c21f740f55cc890cd0c110b01c556dffe083
Ivan Zhang <100223322+RoyleSensei@users.noreply.github.com>
2022-08-07 21:17:08 -0400

    Update Absenteeism_Preprocessing.py

--- Absenteeism_Preprocessing.py
+++ Absenteeism_Preprocessing.py
@@ -3,13 +3,10 @@
 
 # In[2]:
 
-
 import pandas as pd
 
 
 # In[7]:
-
-
 raw_csv_data = pd.read_csv("Absenteeism_data.csv")
 
 raw_csv_data
@@ -17,32 +14,20 @@ raw_csv_data
 
 # In[15]:
 
-
 df = raw_csv_data.copy()
-
-
-# In[14]:
-
-
 df
 
 
 # In[13]:
-
-
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
 
 
 # In[16]:
-
-
 display(df)
 
 
 # In[17]:
-
-
 df.info()
 
 
@@ -57,70 +42,13 @@ type(raw_csv_data)
 
 
 # In[24]:
-
-
 df.drop(['ID'], axis = 1)
 
-
 # In[19]:
-
-
 df
-
 
 # In[22]:
-
-
 df = df.drop(['ID'], axis = 1)
-
-
-# In[27]:
-
-
-df
-
-
-# In[29]:
-
-
-df 
-
-
-# In[ ]:
-
-
-
-
-
-# In[30]:
-
-
-1
-
-
-# In[31]:
-
-
-2
-
-
-# In[33]:
-
-
-3
-
-
-# In[34]:
-
-
-1+1
-
-
-# In[35]:
-
-
-4+5
-
 
 # In[36]:
 
@@ -132,42 +60,35 @@ raw_csv_data
 
 # In[38]:
 
-
 df['Reason for Absence']
 
 
 # In[39]:
-
 
 df['Reason for Absence'].min()
 
 
 # In[40]:
 
-
 df['Reason for Absence'].max()
 
 
 # In[41]:
-
 
 pd.unique(df['Reason for Absence'])
 
 
 # In[42]:
 
-
 df['Reason for Absence'].unique()
 
 
 # In[43]:
 
-
 len(df['Reason for Absence'].unique())
 
 
 # In[44]:
-
 
 sorted(df['Reason for Absence'].unique())
 
@@ -176,7 +97,6 @@ sorted(df['Reason for Absence'].unique())
 
 # In[10]:
 
-
 import pandas as pd
 raw_csv_data = pd.read_csv("Absenteeism_data.csv")
 df = raw_csv_data.copy()
@@ -187,148 +107,71 @@ reason_columns
 
 # In[12]:
 
-
 reason_columns = pd.get_dummies(df['Reason for Absence'])
 
 
 # In[14]:
-
 
 reason_columns['check'] = reason_columns.sum(axis =1)
 
 
 # In[15]:
 
-
 reason_columns
 
 
 # In[16]:
-
 
 reason_columns['check'].sum(axis=0)
 
 
 # In[17]:
 
-
 reason_columns['check'].unique()
 
 
 # In[18]:
-
 
 reason_columns = pd.get_dummies(df['Reason for Absence'], drop_first = True)
 
 
 # In[19]:
 
-
 reason_columns
 
 
 # In[20]:
-
 
 reason_columns['check'] = reason_columns.sum(axis =1)
 
 
 # In[21]:
 
-
 reason_columns
 
 
 # # Group the Reasons for Absence:
 
-# In[2]:
-
-
-import pandas as pd
-
-raw_csv_data = pd.read_csv("Absenteeism_data.csv")
-
-df = raw_csv_data.copy()
-
-df.columns.values
-
-
-# In[3]:
-
-
-reason_columns = pd.get_dummies(df['Reason for Absence'])
-reason_columns
-
-reason_columns.columns.values
-
-
-# In[4]:
-
-
-df = df.drop(['Reason for Absence'], axis = 1)
-
-
-# In[5]:
-
-
-df
-
-
-# In[6]:
-
-
-df = df.drop(['ID'], axis = 1)
-
-
-# In[7]:
-
-
-df
-
-
 # In[8]:
-
 
 reason_columns.loc[:, 15:17]
 
-
 # In[9]:
-
 
 reason_columns.loc[:, 1:14].max(axis = 1)
 
 
 # In[10]:
-
-
 reason_type_1 = reason_columns.loc[:,1:14].max(axis = 1)
 reason_type_2 = reason_columns.loc[:,15:17].max(axis = 1)
 reason_type_3 = reason_columns.loc[:,18:21].max(axis = 1)
 reason_type_4 = reason_columns.loc[:,22:28].max(axis = 1)
 
 
-# In[11]:
-
-
-reason_type_1
-
-
-# In[12]:
-
-
-reason_type_2
-
 
 # # Concatenate Column Values
 
-# In[13]:
-
-
-df
-
-
 # In[14]:
-
 
 df = pd.concat([df, reason_type_1, reason_type_2, reason_type_3, reason_type_4], axis =1)
 df
@@ -336,12 +179,10 @@ df
 
 # In[15]:
 
-
 df.columns.values
 
 
 # In[16]:
-
 
 column_names = ['Date', 'Transportation Expense', 'Distance to Work', 'Age',
        'Daily Work Load Average', 'Body Mass Index', 'Education',
@@ -350,12 +191,10 @@ column_names = ['Date', 'Transportation Expense', 'Distance to Work', 'Age',
 
 # In[17]:
 
-
 df.columns = column_names
 
 
 # In[18]:
-
 
 df.head()
 
@@ -364,7 +203,6 @@ df.head()
 
 # In[19]:
 
-
 column_names_reordered = ['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4','Date', 'Transportation Expense', 'Distance to Work', 'Age',
        'Daily Work Load Average', 'Body Mass Index', 'Education',
        'Children', 'Pets', 'Absenteeism Time in Hours']
@@ -372,12 +210,10 @@ column_names_reordered = ['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4','Date',
 
 # In[20]:
 
-
 df = df[column_names_reordered]
 
 
 # In[21]:
-
 
 df.head()
 
@@ -388,11 +224,6 @@ df.head()
 
 
 df_reason_mod = df.copy()
-
-
-# In[23]:
-
-
 df_reason_mod
 
 
@@ -450,15 +281,11 @@ list_months
 
 
 # In[32]:
-
-
 for i in range(700):
     list_months.append(df_reason_mod['Date'][i].month)
 
 
 # In[33]:
-
-
 list_months
 
 
@@ -470,12 +297,10 @@ len(list_months)
 
 # In[35]:
 
-
 df_reason_mod.shape
 
 
 # In[36]:
-
 
 df_reason_mod['Month Value'] = list_months
 
@@ -623,16 +448,4 @@ df_reason_date_mod['Education'].value_counts()
 
 df_preprocessed=df_reason_date_mod
 df_preprocessed.to_csv('Absenteeism_preprocessed.csv', index=False)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
